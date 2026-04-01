7b98370aac8363e05973c85d0f1bf47d1b8fa059
巫俊彥 <wujunyan@Justin-2025s-gift.local>
2026-04-01 15:09:12 +0700

    fix dist

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: "/joker's-surprise-gift/",
+    base: "/",
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
